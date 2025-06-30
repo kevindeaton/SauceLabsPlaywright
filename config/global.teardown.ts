7ab13755458bc0fb170e5e67e2e5fcc02c2660f6
Kevin Deaton <kdeaton06@gmail.com>
2025-06-30 12:00:16 -0400

    Ran fix command for eslint

--- config/global.teardown.ts
+++ config/global.teardown.ts
@@ -3,11 +3,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 test('Logout and clear Session', async () => {
-    
-    // Writes an empty object to the auth.json file to clear session data
-    const testData = {};
-    const filePath = path.resolve(__dirname, './auth.json');
-    fs.writeFileSync(filePath, JSON.stringify(testData, null, 2), 'utf-8');
+  // Writes an empty object to the auth.json file to clear session data
+  const testData = {};
+  const filePath = path.resolve(__dirname, './auth.json');
+  fs.writeFileSync(filePath, JSON.stringify(testData, null, 2), 'utf-8');
 
-    console.log(`Test data written to ${filePath}`);
+  console.log(`Test data written to ${filePath}`);
 });
--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,42 +1,44 @@
-import { test, expect } from '../pages/fixtures/fixtures.ts';
+import { test, expect } from '../pages/fixtures/fixtures.page.ts';
 
 test('Inventory Items should contain all the correct information', async ({ inventoryPage }) => {
+  inventoryPage.navigateTo();
 
-    inventoryPage.navigateTo();
+  for (const item of inventoryItems) {
+    // Assert the 'Add To Cart' button is visible
+    const button = inventoryPage.getButtonByLabelText(item.name);
+    await expect(button).toBeVisible();
 
-    for (const item of inventoryItems) {
-        // Assert the 'Add To Cart' button is visible
-        const button = inventoryPage.getButtonByLabelText(item.name);
-        await expect(button).toBeVisible();
+    // Assert the image is visible
+    const image = inventoryPage.getImageByLabelText(item.name);
+    await expect(image).toBeVisible();
 
-        // Assert the image is visible
-        const image = inventoryPage.getImageByLabelText(item.name);
-        await expect(image).toBeVisible();
+    // Assert the description is correct
+    const description = inventoryPage.getDescriptionByLabelText(item.name);
+    await expect(description).toHaveText(item.description);
 
-        // Assert the description is correct
-        const description = inventoryPage.getDescriptionByLabelText(item.name);
-        await expect(description).toHaveText(item.description);
-
-        // Assert the price is correct
-        const price = inventoryPage.getPriceByLabelText(item.name);
-        await expect(price).toHaveText(item.price)
-    }
+    // Assert the price is correct
+    const price = inventoryPage.getPriceByLabelText(item.name);
+    await expect(price).toHaveText(item.price);
+  }
 });
 
 const inventoryItems = [
-    {
-        name: 'Sauce Labs Backpack',
-        price: '$29.99',
-        description: 'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
-    },
-    {
-        name: 'Sauce Labs Bike Light',
-        price: '$9.99',
-        description: 'A red light isn\'t the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.',
-    },
-    {
-        name: 'Sauce Labs Bolt T-Shirt',
-        price: '$15.99',
-        description: 'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
-    }
-]
+  {
+    name: 'Sauce Labs Backpack',
+    price: '$29.99',
+    description:
+      'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
+  },
+  {
+    name: 'Sauce Labs Bike Light',
+    price: '$9.99',
+    description:
+      "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
+  },
+  {
+    name: 'Sauce Labs Bolt T-Shirt',
+    price: '$15.99',
+    description:
+      'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
+  },
+];
